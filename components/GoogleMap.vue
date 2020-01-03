b7c3fc84cce73173480e623b6f4bc170599304c7
Elliott0121 <44977939+Elliott0121@users.noreply.github.com>
2020-01-03 23:30:17 +0100

    Skapat koordinater på kartan med exempel tweets

--- components/GoogleMap.vue
+++ components/GoogleMap.vue
@@ -97,6 +97,51 @@ module.exports = {
           ]
     }
     const map = new google.maps.Map(element, options);
+    map.data.loadGeoJson("map.json");
+
+    var image = {
+        url: "https://pbs.twimg.com/profile_images/875168599299637248/84CkAq6s.jpg",
+        scaledSize: new google.maps.Size(25, 25)
+    }
+
+    var markers = [
+      {
+        coords: {lat:59.329323, lng:18.068581},
+        content:"<p>Testar123</p>",
+        iconImage: image
+      },
+      {
+        coords: {lat:55.603310, lng:13.001310},
+        content:"<p>Tweet från malmö</p>",
+        iconImage: image
+      }
+    ];
+
+    //Går igenom markers i arrayen och placerar ut dom.
+    for (var i = 0; i < markers.length; i++){
+      tweetMarker(markers[i]);
+    }
+
+    function tweetMarker(props){
+      var marker = new google.maps.Marker({
+        position: props.coords,
+        map: map
+      });
+      
+      if (props.iconImage){
+        marker.setIcon(props.iconImage);
+      }
+
+      if (props.content){
+      var infoWindow = new google.maps.InfoWindow({
+        content: "<i class=" + "material-icons" + ">" + "person" + "</i>" + "<p>" + props.content + "</p>"
+      });
+
+      marker.addListener("click", function(){
+        infoWindow.open(map, marker);
+      });
+      }
+    }
   }
 };
 </script>
--- components/TweetMap.vue
+++ components/TweetMap.vue
@@ -14,9 +14,9 @@
         <main class="container">
             <div class="row">
                 <div class="col s12 m12 center">
-                    <i class="medium material-icons">group</i>
+                    <i class="medium material-icons">map</i>
                     <p class="promo-caption">TweetMap</p>
-                    <p class="light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia ratione corrupti assumenda vel. Itaque, quis, nobis eius tenetur incidunt quidem distinctio earum dolor, ullam ex cumque sed. Doloribus, error rerum?</p>
+                    <p class="promo-caption">Vad säger folk på twitter om politiker runt om Sverige?</p>
                     <google-map
                     name="example"
                     ></google-map>
